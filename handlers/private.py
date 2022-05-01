4b5fd08e23359cc97ea5e27dc77a1052f81113c3
Kartikboss09 <104703880+Kartikboss09@users.noreply.github.com>
2022-05-01 09:55:33 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -13,19 +13,15 @@ async def start_(client: Client, message: Message):
         f start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/c79c552994c222362d80e.jpg",
-        caption=f"""**💥 ʜᴇʟʟᴏ, ɪ ᴀᴍ ʟᴇɢᴇɴᴅ ᴍᴜꜱɪᴄ ʙᴏᴛ
-ɪ ᴀᴍ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ
-ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ᴛᴏ ᴘʟᴀʏ ᴀᴜᴅɪᴏ ᴀɴᴅ ᴠɪᴅᴇᴏ sᴏɴɢ..
-═══════════════════
-★ sᴜᴘᴘᴏʀᴛ ›› @ROYALUBOT_SUPPORT
-★ ᴜᴘᴅᴀᴛᴇs ›› @ROYALYSERBOT
-★ ᴄʀᴇᴀᴛᴏʀ ›› @ROYALBOY_XD
-★ ᴅᴇᴠᴇʟᴏᴘᴇʀ ›› @ItsMeViju
-★ ᴏᴡɴᴇʀ ›› @KartiK_KinG01
-═══════════════════
-[Join Here For Chit Chat 😁](https://t.me/ved_maitrich007)
-   💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ
-ᴅᴍ ᴛᴏ ᴍʏ [ʟᴇɢᴇɴᴅ ᴏᴡɴᴇʀ](t.me/KartiK_KinG01) ...**""",
+        caption=f"""**......⭕𝙎𝙪𝙥𝙥𝙤𝙧𝙩:-  @ROYALUBOT_SUPPORT
+       ⭕𝘾𝙍𝙀𝘼𝙏𝙊𝙍:- @ROYALBOY_XD
+       ⭕𝘾𝙝𝙖𝙣𝙣𝙚𝙡:- @ROYALYSERBOT
+       ⭕𝙊𝙒𝙉𝙀𝙍:- @ROYALBOY_XD
+       ⭕𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧:- @KartiK_KinG01
+🌟👨‍🎤𝙅𝙊𝙄𝙉 𝙊𝙐𝙍 𝙏𝙀𝘼𝙈 𝙉𝙊𝙒
+
+ 𝙁𝙊𝙍 𝘾𝙃𝘼𝙏𝙏𝙄𝙉𝙂💌 𝙅𝙊𝙄𝙉 - @ved_maitrich007
+   𝙄𝙛 𝙔𝙤𝙪 𝙣𝙚𝙚𝙙 𝙖𝙣𝙮 𝙝𝙚𝙡𝙥 𝙩𝙝𝙚𝙣 𝙠𝙞𝙣𝙙𝙡𝙮 𝙘𝙤𝙣𝙩𝙖𝙘𝙩 𝙩𝙤 [👨‍💻𝙇𝙀𝙂𝙀𝙉𝘿👨‍💻](t.me/KartiK_KinG01) ...**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
